a7e09c8f3de86e990fe9d5048b27367309ddd898
Vikas Vimal <vikas@joinzorro.com>
2023-11-19 01:17:13 +0530

    added streaming

--- client.js
+++ client.js
@@ -5,55 +5,53 @@ const grpcObject = grpc.loadPackageDefinition(packageDef);
 const todoPackage = grpcObject.todoPackage;
 
 const client = new todoPackage.TodoService(
-  "localhost:4040",
+  "localhost:4050",
   grpc.credentials.createInsecure()
 );
 
-let myTodoList = [];
+const ACTION = process.argv[2] || null;
+const VALUE = process.argv[3] || null;
+const ID = process.argv[4] || null;
+console.log(`---- ~ action : value : id :`, ACTION, VALUE, ID);
 
-client.getAllTodos({}, (err, res) => {
-  myTodoList = res?.todos || [];
-  console.log("getAllTodos response", myTodoList);
+switch (ACTION) {
+  case "c":
+    console.log("creating todo");
+    client.createTodo(
+      {
+        text: VALUE,
+      },
+      (err, res) => {
+        console.log("createTodo response", { err, res });
+      }
+    );
+    break;
+  case "u":
+    console.log("updating todo");
+    client.updateTodo(
+      {
+        text: VALUE,
+        id: ID,
+      },
+      (err, res) => {
+        console.log("updateTodo response", { err, res });
+      }
+    );
+    break;
+  case "r":
+    console.log("reading all todos");
+    client.getAllTodos({}, (err, res) => {
+      console.log("getAllTodo response", res?.todos);
+    });
+    break;
+
+  default:
+    console.log("no actions");
+    break;
+}
+
+const streamCall = client.streamTodos();
+streamCall.on("data", (item) => {
+  console.log("stream received todo: ", item);
 });
-
-client.updateTodo(
-  {
-    id: myTodoList?.[1]?.id || 3,
-    text: "Drink Juice",
-  },
-  (err, res) => {
-    console.log("updateTodo response", { err, res });
-  }
-);
-
-client.getAllTodos({}, (err, res) => {
-  myTodoList = res?.todos || [];
-  console.log("getAllTodos response", myTodoList);
-});
-
-client.createTodo(
-  {
-    text: "Eat Apple",
-  },
-  (err, res) => {
-    console.log("createTodo response", { err, res });
-  }
-);
-
-client.createTodo(
-  {
-    text: "Drink Joice",
-  },
-  (err, res) => {
-    console.log("createTodo response", { err, res });
-  }
-);
-
-client.createTodo(
-  {
-    text: "Eat banana",
-  },
-  (err, res) => {
-    console.log("createTodo response", { err, res });
-  }
-);
+streamCall.on("end", (e) => console.log("stream ended"));
--- server.js
+++ server.js
@@ -5,13 +5,14 @@ const grpcObject = grpc.loadPackageDefinition(packageDef);
 const todoPackage = grpcObject.todoPackage;
 
 const server = new grpc.Server();
-server.bind("0.0.0.0:4040", grpc.ServerCredentials.createInsecure());
+server.bind("0.0.0.0:4050", grpc.ServerCredentials.createInsecure());
 
 server.addService(todoPackage.TodoService.service, {
-  createTodo: createTodo,
-  updateTodo: updateTodo,
-  deleteTodo: deleteTodo,
-  getAllTodos: getAllTodos,
+  createTodo,
+  updateTodo,
+  deleteTodo,
+  getAllTodos,
+  streamTodos,
 });
 
 const db = {
@@ -22,11 +23,11 @@ server.start();
 
 function createTodo(call, callback) {
   console.log(`---- ~ create todo call request:`, call.request);
-  const id = call.request?.id;
   const text = call.request?.text || "";
+  console.log("create todo call", { text });
   if (!text) callback(null, null);
   const todoItem = {
-    id: id || new Date().getTime(),
+    id: db.todoList.length + 1,
     text,
   };
   console.log(`---- ~ create todoItem:`, todoItem);
@@ -54,4 +55,9 @@ function getAllTodos(call, callback) {
   callback(null, {
     todos: db.todoList,
   });
+}
+
+function streamTodos(call, callback) {
+  db.todoList.forEach((item) => call.write(item));
+  call.end();
 }
